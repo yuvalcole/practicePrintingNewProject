88427e10c7c8e30aa5371329f02786a1d2ddbe58
Yuval <yuval.cole@gmail.com>
2018-09-25 11:19:57 +0200

    Maze correct now

--- MagicMaze.py
+++ MagicMaze.py
@@ -3,6 +3,7 @@ moves = 0
 lifes = 3
 direction = ""
 correctCombination = "SSNWES"
+a=0;
 while flag:
     prompt = "Which way do you want to go? (N, S , W, E )"
     election = input(prompt)
@@ -13,14 +14,16 @@ while flag:
 
     elif election == "N" or election == "S" or election =="W" or election == "E":
 
-        if(election.__contains__(correctCombination)):
+        if(election==correctCombination[a]):
             direction = direction + election
             print("Good choice, you are now one step closer!")
             moves +=1
+            a+=1
 
         else:
             print("Try again")
             moves+=1
+            a=0
 
     if moves%10==0:
         lifes -= 1
@@ -28,8 +31,11 @@ while flag:
             ("Game Over, you lost all your lifes")
             flag = False
 
+
     if election == correctCombination:
         print ("GG")
+        print("You did it in", moves, "moves.")
+        print("You had", lifes, "lifes left.")
 
 
 
